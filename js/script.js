efc13c9d66594d3d9588da4e94ba20a36c23fd5a
Andrew Bjork <bjorkandrew1@gmail.com>
2020-02-01 11:45:18 -0600

    changing out js

--- js/script.js
+++ js/script.js
@@ -7,76 +7,29 @@ var objectTest = {
         LocationTest: "Califorincation",
         Weather: "'It's Raining Men",
     }
-
 }
-
 let keys = Object.keys(objectTest);
 //console.log(objectTest[newI].LocationTest);
 $(document).ready(function () {
-
     let keys = Object.keys(objectTest);
     for (var i = 0; i < keys.length; i++) {
-
         var createNewOption = $("<option>");
         createNewOption.addClass("autoOption");
         createNewOption.attr("id", objectTest[keys[i]].LocationTest);
         createNewOption.attr("value", objectTest[keys[i]].LocationTest);
         $(createNewOption).text(objectTest[keys[i]].LocationTest);
         //console.log(objectTest[keys[i]].LocationTest);
-
         $(".skill-dropdown").append(createNewOption);
     }
-
 });
-
 // Add click functions to Search
-
 $(document).ready(function () {
-
-
     $(".button").click(function () {
         var searchBar = $("#search").val();
-<<<<<<< HEAD
-
-        function getVideo() {
-            $.ajax({
-                type: 'GET',
-                url: 'https://www.googleapis.com/youtube/v3/search',
-                data: {
-                    key: 'AIzaSyBlRMCifkXmDv30ugkOCWMX3OkgtpNseR4',
-                    q: "How to " + searchBar,
-                    part: 'snippet',
-                    maxResults: 1,
-                    type: 'video',
-                    videoEmbeddable: true,
-                },
-                success: function (data) {
-                    embedVideo(data)
-                },
-                error: function (response) {
-                    console.log("Request Failed");
-                }
-            });
-
-            if ($("#checkbox12").prop("checked") == true) {
-
-            }
-        
-
-        function embedVideo(data) {
-            $('iframe').attr('src', 'https://www.youtube.com/embed/' + data.items[0].id.videoId)
-            $('h3').text(data.items[0].snippet.title)
-            // $('.description').text(data.items[0].snippet.description)
-        }
-
-        getVideo();
-        if ($("#checkbox22").prop("checked") == true) {
-=======
         console.log(searchBar);
         console.log("second " + searchBar);
         if ($("#youtube-radio").prop("checked") == true) {
             getVideo(searchBar);
->>>>>>> d6ee9345c0cb87f0e910a9199ccb579a1a6277aa
         };
         if ($("#wiki-radio").prop("checked") == true) {
             getInfo(searchBar);
@@ -84,28 +37,14 @@ $(document).ready(function () {
         if ($("#nearby-radio").prop("checked") == true) {
             getDefinition(searchBar);
         };
-
         clear();
-
         var queryURL = getDefinition();
-
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(updatePage);
-
     });
-*/
-
-    /*
-
 });
-
-
-
-
-
-
 //list of functions
 //get the video
 function getVideo(searchBar) {
@@ -129,14 +68,12 @@ function getVideo(searchBar) {
         }
     });
 }
-
 //puts video to page
 function embedVideo(data) {
     $('iframe').attr('src', 'https://www.youtube.com/embed/' + data.items[0].id.videoId)
     $('h3').text(data.items[0].snippet.title)
     // $('.description').text(data.items[0].snippet.description)
 }
-
 //API AJAX for wiki
 function getInfo() {
     $.ajax({
@@ -153,24 +90,19 @@ function getInfo() {
         }
     });
 }
-
 //clrears previous info
 function clear() {
     $("#def").empty();
 }
-
 //Dictionary function
 function getDefinition() {
-
     var queryURL = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/";
     var queryParams = { "key": "00129733-b164-4319-9aef-fb3e4f691bac" };
     DefSearch = searchBar
-
     console.log("---------------\nURL: " + queryURL + "\n---------------");
     console.log(queryURL + DefSearch + "?" + $.param(queryParams));
     return queryURL + DefSearch + "?" + $.param(queryParams);
 }
-
 //updates the page to new info
 function updatePage(defData) {
     var mainDef = defData[0].meta.id
@@ -178,12 +110,10 @@ function updatePage(defData) {
     var defType = defData[0].fl
     var definition = defData[0].def[0].sseq[0][1][1].dt[0][1]
     console.log(definition)
-
     var displayDef = $("<div></div>").text(mainDef)
     var displayPron = $("<div></div>").text(defPron)
     var typeDef = $("<div></div>").text(defType)
     var defi = $("<div></div>").text(definition)
-
     $("#def").append(displayDef)
     $("#def").append(displayPron)
     $("#def").append(typeDef)
